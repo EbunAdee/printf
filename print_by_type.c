025e18c0ae1cbe46759630790ffa43000af2fcd9
op-pixel <horpheoluhwah@gmail.com>
2023-07-01 12:18:46 -0700

    printf

--- print_by_type.c
+++ print_by_type.c
@@ -16,11 +16,13 @@ int handle_print_type(const char *frmt, int *idx, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int a, unknwlen = 0, printed_chars = -1;
-	frmt_t frmt_types[] = {{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octa}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}};
+	frmt_t frmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},{'u', print_unsigned},
+		{'o', print_octa}, {'x', print_hexadecimal},{'X', print_hexa_upper},
+		{'p', print_pointer},{'S', print_non_printable},{'r', print_reverse},
+		{'R', print_rot13string},{'\0', NULL}
+	};
 
 	for (a = 0; frmt_types[a].frmt != '\0'; a++)
 		if (frmt[*idx] == frmt_types[a].frmt)
